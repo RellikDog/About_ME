7ff6d9bc0a69d5004937456d8d3b06a74cc8bc46
Richard Bellamy <richard.bellamy123@gmail.com>
2018-09-11 18:12:43 -0700

    testing first message and that all pages are linked

---
+++ js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//welcome user to my page and ask their name
+var userName=prompt('Welcome to my about me page! What should I call you?');
+    alert('Nice to meet you '+userName+'. Since this is an about me page why dont you try to guess a few things about me?');
+    
