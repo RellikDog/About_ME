ed5931f16d221971dea6ac0f3330815a637b6c7a
Richard Bellamy <richard.bellamy123@gmail.com>
2018-09-11 19:43:52 -0700

    testing q4

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 //welcome user to my page and ask their name
 var userName=prompt('Welcome to my favorite foods page! What should I call you?');
-    alert('Nice to meet you '+userName+'. Since this is a page about my favorite foods why dont you try to guess a few things about me?');
+    alert('Nice to meet you '+userName+'. Since this is a page about my favorite foods why dont you try to guess a few things about me? Please guess with yes/no or y/n.');
         console.log('visitors name: '+userName);
 
 //Do I love pizza?
@@ -23,3 +23,13 @@ var hawPiz=prompt('Not so hard because who doesn\'t love pizza? But do you think
             }else{
                 alert('Are you even trying?');
                 }
+                console.log('Do they think I like hawian pizza: '+hawPiz);
+
+//Do I like sushi?
+var sushiLove=prompt('OK lets try another food. Do  you think I like sushi?');
+    if(sushiLove.toUpperCase()==='YES'||sushiLove.toUpperCase()==='Y'){
+      alert('You are correct '+userName+'!');  
+    }else{
+        alert('You have answered incorrectly '+userName);
+        }
+        console.log('Do they think I like sushi?: '+sushiLove);
