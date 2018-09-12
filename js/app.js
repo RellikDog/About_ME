e375728831b40e77214b9585a3e8d066b3699adb
Richard Bellamy <richard.bellamy123@gmail.com>
2018-09-11 18:53:15 -0700

    testing q2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 'use strict';
 
 //welcome user to my page and ask their name
-var userName=prompt('Welcome to my about me page! What should I call you?');
-    alert('Nice to meet you '+userName+'. Since this is an about me page why dont you try to guess a few things about me?');
-    
+var userName=prompt('Welcome to my favorite foods page! What should I call you?');
+    alert('Nice to meet you '+userName+'. Since this is a page about my favorite foods why dont you try to guess a few things about me?');
+        console.log('visitors name: '+userName);
+
+//Do I love pizza?
+var pizzalove=prompt('Do you think I love pizza?');
+    if(pizzaLove.toUpperCase()==='YES'||pizzaLove.toUpperCase()==='Y'){
+        alert('Of course!');
+    } else {
+    alert('...no way...');
+        } 
